3b84adaaa7814808d4319ade3706742295de948d
mjackson89 <marcusj98@gmail.com>
2026-02-24 23:14:16 -0600

    #8 Added testcases

--- tests/test_prompt_layer.py
+++ tests/test_prompt_layer.py
@@ -7,3 +7,10 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 
 from src.layers.prompt_layer import PromptLayer
+
+def test_prompt_layer_exit():
+    with pytest.raises(SystemExit) as e:
+        sys.exit(0)
+    
+    assert e.type is SystemExit
+    assert e.value.code == 0
