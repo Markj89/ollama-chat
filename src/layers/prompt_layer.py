a1767770f55187b51e129f948bce5317c04c21c9
mjackson89 <marcusj98@gmail.com>
2026-06-12 15:19:04 -0500

    API Key Setup Fixes #19

--- src/layers/prompt_layer.py
+++ src/layers/prompt_layer.py
@@ -6,13 +6,15 @@ def PromptLayer(client, model) -> list[dict]:
     """Continue the conversation:"""
     user_input = set_prompt("You: ", type=str)
     answer = generate_response(client=client, model=model, user_input=user_input)
-    print("Bot: ", answer, end="\n")
+    print("Bot: ", answer, end="\n", flush=True)
 
 def generate_response(client, model, user_input) -> list[dict]:
+    answer = ''
     conversation = Conversation()
     conversation.add_user(sanitize_input(user_input))
     user_prompt = conversation.serialize()
-    response = client.chat(model=model, messages=user_prompt, stream=False)
-    answer = response['message']['content']
+    response = client.chat(model=model, messages=user_prompt, stream=True)
+    for res in response:
+        answer += res['message']['content']
     conversation.add_assistant(answer)
     return answer
